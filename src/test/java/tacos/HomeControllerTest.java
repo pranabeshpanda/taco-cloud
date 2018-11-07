5b4459168436c46315af9f9d8ce14f700fd565bb
riskimidiw <riskimidiw@gmail.com>
2018-11-07 08:52:50 +0700

    Update unit testing for DesignTacoController, HomeController

--- src/test/java/tacos/HomeControllerTest.java
+++ src/test/java/tacos/HomeControllerTest.java
@@ -14,6 +14,7 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.view;
 
 @RunWith(SpringRunner.class)
+@WebMvcTest
 public class HomeControllerTest {
 
     @Autowired
